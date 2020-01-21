88fedf31603e8180e7575f70d8071c708d6d0e22
ICEZ <45548846+icecooler112@users.noreply.github.com>
2020-01-21 15:14:49 +0700

    commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Dashboard</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+            <?php if(isset($_SESSION['id'])) { ?>
+            <center><h5><?php echo $_SESSION["First_Name"];?> <?php echo $_SESSION["Last_Name"];?></h5></center>
+            <?php }else header("location:login.php"); ?>
+                
+                
+            </div>
+
+            <ul class="list-unstyled components">
+                <li class="active">
+                    <a href="index.php">Dashboard</a>
+                </li>
+                <li>
+                    <a href="user_list.php">User List</a>
+                </li>
+                <li>
+                    <a href="rp_history.php">Repair history</a>
+                </li>
+                <li>
+                    <a href="product.php">Products</a>
+                </li>
+            </ul>
+
+            <ul class="list-unstyled CTAs">
+                <li>
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#LogoutModal">
+              <i class="material-icons">Logout</i>
+            </a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="LogoutModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-dark">Logout ?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body text-center">
+          <h1 style="font-size:5.5rem;"><i class="fa fa-sign-out text-danger" aria-hidden="true"></i></h1>
+          <p class="text-dark">Are you sure you want to log-out?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                   
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <p><h4>Dashboard</h4></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
---
+++ login.php
@@ -0,0 +1,93 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login</title>
+    <!-- ติดตั้งการใช้งาน CSS ต่างๆ -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <?php
+        require_once('include/connect.php'); // ดึงไฟล์เชื่อมต่อ Database เข้ามาใช้งาน
+        /**
+         * ตรวจสอบเงื่อนไขที่ว่า ตัวแปร $_POST['submit'] ได้ถูกกำหนดขึ้นมาหรือไม่
+         */
+        if (isset($_POST['submit'])) { 
+            /**
+             * กำหนดตัวแปรเพื่อมารับค่า
+             */
+            $username =  $conn->real_escape_string($_POST['username']);
+            $password = $conn->real_escape_string($_POST['password']);
+            /**
+             * สร้างตัวแปร $sql เพื่อเก็บคำสั่ง Sql
+             * จากนั้นให้ใช้คำสั่ง $conn->query($sql) เพื่อที่จะประมาณผลการทำงานของคำสั่ง sql
+             */
+            $sql = "SELECT * FROM `admin_lg` WHERE `username` = '".$username."' AND `password` = '".$password."'";
+            $result = $conn->query($sql);
+
+            /**
+             * ตรวจสอบการเข้าสู่ระบบ
+             */
+            if($result->num_rows > 0){
+                /**
+                 * แสดงข้อมูลของ user 
+                 * เก็บข้อมูลเข้าสู่ session เพื่อนำไปใช้งาน 
+                 */
+                $row = $result->fetch_assoc();
+                $_SESSION['id'] = $row['id'];
+                $_SESSION['First_Name'] = $row['First_Name'];
+                $_SESSION['Last_Name'] = $row['Last_Name'];
+                header('location:index.php');
+            }else{
+              echo "<script>";
+        echo "alert(\" Username หรือ  Password ของคุณไม่ถูกต้อง\");";
+        echo "</script>";
+        header('Refresh:0; url=login.php');
+            } 
+        }
+    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 mx-auto mt-3">
+                <div class="card">
+                    <form action="" method="POST">           
+                        <div class="card-header text-center">
+                        <h3><a><b>Admin</b>Login</a></h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group row">
+                                <label for="username" class="col-sm-3 col-form-label">Username</label>
+                                <div class="col-sm-9">
+                                    <input type="text" class="form-control" id="username" name="username" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="password" class="col-sm-3 col-form-label">Password</label>
+                                <div class="col-sm-9">
+                                    <input type="password" class="form-control" id="password" name="password" required>
+                                </div>    
+                            </div>
+                        </div>
+                        <div class="card-footer text-center">
+                            <input type="submit" name="submit" class="btn btn-primary" value="Login">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ติดตั้งการใช้งาน Javascript ต่างๆ -->    
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+
+</body>
+
+</html>
